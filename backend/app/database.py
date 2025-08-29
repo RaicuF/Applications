645ab9a12e9325f59f2fb88f69ba5b74e3c43086
Florin Raicu <florin-constantin1.raicu-external@rbinternational.com>
2025-08-29 13:48:15 +0300

    add new lines

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text, Float, Boolean, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session, relationship
+from sqlalchemy import Column, Integer, String, DateTime, Text, Float, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, Session, relationship
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 import os
 from datetime import datetime
